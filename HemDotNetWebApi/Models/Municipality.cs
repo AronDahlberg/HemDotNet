bff79f3c72b7e9e3f939d256dc0eac9f1a8fd495
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-04-09 11:56:11 +0200

    Added Municipality, Image, Agent, and Agency models.

---
+++ HemDotNetWebApi/Models/Municipality.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HemDotNetWebApi.Models
+{
+    public class Municipality
+    {
+        [Key]
+        public int MunicipalityId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string MunicipalityName { get; set; }
+
+        [NotMapped]
+        public virtual List<MarketProperty> MunicipalityProperties { get; set; }
+    }
+}
---
+++ HemDotNetWebApi/Models/RealEstateAgent.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HemDotNetWebApi.Models
+{
+    public class RealEstateAgent
+    {
+        [Key]
+        public int RealEstateAgentId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string RealEstateAgentFirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string RealEstateAgentLastName { get; set; }
+
+        [Required]
+        public string RealEstateAgentEmail { get; set; }
+
+        [Required]
+        public string RealEstateAgentPhoneNumber { get; set; }
+
+        [Required]
+        public string RealEstateAgentImageUrl { get; set; }
+
+        [Required]
+        [ForeignKey("RealEstateAgency")]
+        public RealEstateAgency RealEstateAgentAgency { get; set; }
+
+        [NotMapped]
+        public virtual List<MarketProperty> RealEstateAgentProperties { get; set; }
+    }
+}
