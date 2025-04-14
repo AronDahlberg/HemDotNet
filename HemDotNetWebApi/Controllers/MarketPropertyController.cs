afd886f59862ca81041c1e19c1381cb6f15b88d1
Ordinant <Ordinant@DESKTOP-O6SI4Q1>
2025-04-14 17:00:35 +0200

    Added field for Repository and GetAllMarketProperties method to MarketPropertiesController.

--- HemDotNetWebApi/Controllers/MarketPropertyController.cs
+++ HemDotNetWebApi/Controllers/MarketPropertyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HemDotNetWebApi.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HemDotNetWebApi.Controllers
@@ -7,5 +8,19 @@ namespace HemDotNetWebApi.Controllers
     [ApiController]
     public class MarketPropertyController : ControllerBase
     {
+        private readonly IMarketPropertyRepository _marketPropertyRepository;
+
+        public MarketPropertyController(IMarketPropertyRepository marketPropertyRepository)
+        {
+            _marketPropertyRepository = marketPropertyRepository;
+        }
+        //Author Johan Ek
+        [HttpGet]
+        public async Task<IActionResult> GetAllMarketProperties()
+        {
+            var marketProperties = await _marketPropertyRepository.GetAllMarketProperties();
+            return Ok(marketProperties);
+        }
+
     }
 }
