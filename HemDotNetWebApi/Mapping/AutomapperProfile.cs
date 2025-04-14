3a3078dfc9cb4f140276cb500188a19a1cff07d2
Allan Crépin <alllan2107@gmail.com>
2025-04-14 12:32:18 +0200

    Added empty controllers and an empty automapper class to avoid later merge conflicts.

---
+++ HemDotNetWebApi/Mapping/AutomapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace HemDotNetWebApi.Mapping
+{
+    public class AutomapperProfile : Profile
+    {
+
+        public AutomapperProfile()
+        {
+            
+        }
+    }
+}
