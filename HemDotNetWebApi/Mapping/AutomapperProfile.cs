f116a9cdc3f5cd865a893030f7086d8f42377a9f
Chris <christian.bang@gmail.com>
2025-04-15 10:58:30 +0200

    mapping for minimal dto

--- HemDotNetWebApi/Mapping/AutomapperProfile.cs
+++ HemDotNetWebApi/Mapping/AutomapperProfile.cs
@@ -1,13 +1,17 @@
 ﻿using AutoMapper;
+using HemDotNetWebApi.DTOs;
+using HemDotNetWebApi.Models;
 
 namespace HemDotNetWebApi.Mapping
 {
     public class AutomapperProfile : Profile
     {
-
         public AutomapperProfile()
         {
-            
+
+            CreateMap<MarketProperty, ActiveMarketListingDTO>()
+                .ForMember(dest => dest.MarketPropertyId, opt => opt.MapFrom(src => src.MarketPropertyId))
+                .ForMember(dest => dest.PropertyAddress, opt => opt.MapFrom(src => src.PropertyAddress));
         }
     }
 }
