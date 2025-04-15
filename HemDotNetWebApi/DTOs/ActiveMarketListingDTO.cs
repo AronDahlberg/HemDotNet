d8a24e4eff89bc05cc2c661db9289f3ff2c9105e
Chris <christian.bang@gmail.com>
2025-04-15 10:57:44 +0200

    minimal dto

---
+++ HemDotNetWebApi/DTOs/ActiveMarketListingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HemDotNetWebApi.DTOs
+{
+
+    // Chris: Minimal Data about Property (Active Market Listing)
+    public class ActiveMarketListingDTO
+    {
+        public int MarketPropertyId { get; set; }
+        public string PropertyAddress { get; set; }
+    }
+}
