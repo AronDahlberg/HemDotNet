fb7fbabb2e0c575e45a4be01e83bb76f64b32d1a
Allan Crépin <alllan2107@gmail.com>
2025-04-15 15:01:54 +0200

    Added constructor

--- HemDotNetWebApi/Controllers/MarketPropertyController.cs
+++ HemDotNetWebApi/Controllers/MarketPropertyController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using HemDotNetWebApi.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HemDotNetWebApi.Controllers
@@ -7,5 +9,15 @@ namespace HemDotNetWebApi.Controllers
     [ApiController]
     public class MarketPropertyController : ControllerBase
     {
+        private readonly IMapper _mapper;
+        private readonly IMarketPropertyRepository _marketPropertyRepository;
+
+        public MarketPropertyController(IMapper mapper, IMarketPropertyRepository marketPropertyRepository)
+        {
+            _mapper = mapper;
+            _marketPropertyRepository = marketPropertyRepository;
+        }
+
+
     }
 }
