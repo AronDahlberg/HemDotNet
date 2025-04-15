9e2ccbd2c91930725245da394d58718614c58260
Chris <christian.bang@gmail.com>
2025-04-15 13:40:02 +0200

    moar data

--- HemDotNetWebApi/DTOs/ActiveMarketListingDTO.cs
+++ HemDotNetWebApi/DTOs/ActiveMarketListingDTO.cs
@@ -1,10 +1,16 @@
-﻿namespace HemDotNetWebApi.DTOs
-{
+﻿using HemDotNetWebApi.Common;
+using HemDotNetWebApi.Models;
 
+namespace HemDotNetWebApi.DTOs
+{
     // Chris: Minimal Data about Property (Active Market Listing)
     public class ActiveMarketListingDTO
     {
         public int MarketPropertyId { get; set; }
+        public Municipality Municipality { get; set; }
+        public PropertyCategory Category { get; set; }
+        public virtual List<PropertyImage> Images { get; set; }
         public string PropertyAddress { get; set; }
+        public decimal Price { get; set; }
     }
 }
