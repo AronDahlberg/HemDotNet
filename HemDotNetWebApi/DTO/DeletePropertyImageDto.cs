d7d5c4eef03cda75f9bf9df183b1685b6a1bd62e
Allan Crépin <alllan2107@gmail.com>
2025-04-17 14:58:48 +0200

    Added DeletePropertyImageDto

---
+++ HemDotNetWebApi/DTO/DeletePropertyImageDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HemDotNetWebApi.DTO
+{
+    public class DeletePropertyImageDto
+    {
+        [Required]
+        public int PropertyImageId { get; set; }
+    }
+}
