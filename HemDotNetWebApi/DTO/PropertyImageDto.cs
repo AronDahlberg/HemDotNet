986ac0b0968e9d10ff5039bbc6017c6503457bf4
Allan Crépin <alllan2107@gmail.com>
2025-04-17 14:57:09 +0200

    Modified PropertyImageDto

--- HemDotNetWebApi/DTO/PropertyImageDto.cs
+++ HemDotNetWebApi/DTO/PropertyImageDto.cs
@@ -3,7 +3,8 @@
     // Allan
     public class PropertyImageDto
     {
-        public int? PropertyImageId { get; set; }
+        public int PropertyImageId { get; set; }
         public string ImageUrl { get; set; }
+        public int MarketPropertyId { get; set; }
     }
 }
