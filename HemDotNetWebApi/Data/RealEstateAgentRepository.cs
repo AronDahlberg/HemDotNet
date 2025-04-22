d1d8f6a87cf1cf890b5a984df3bedfe5fb63b40d
Chris <christian.bang@gmail.com>
2025-04-22 09:30:56 +0200

    add get and constructor for context

--- HemDotNetWebApi/Data/RealEstateAgentRepository.cs
+++ HemDotNetWebApi/Data/RealEstateAgentRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace HemDotNetWebApi.Data
+﻿using AutoMapper;
+using HemDotNetWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HemDotNetWebApi.Data
 {
     public class RealEstateAgentRepository : IRealEstateAgentRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public RealEstateAgentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<RealEstateAgent>> Get(int agentId)
+        {
+            return await _context.RealEstateAgents
+                .Where(p => p.RealEstateAgentId == agentId)
+                .ToListAsync();
+        }
     }
 }
