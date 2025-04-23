4ecea452820ebfbbc979276f27d9bbf03be59d8d
Chris <christian.bang@gmail.com>
2025-04-23 10:28:46 +0200

    mapper profiles for RealEstateAgentUpdateDTO and RealEstateAgent

--- HemDotNetWebApi/Mapping/AutomapperProfile.cs
+++ HemDotNetWebApi/Mapping/AutomapperProfile.cs
@@ -33,6 +33,12 @@ namespace HemDotNetWebApi.Mapping
             CreateMap<MarketProperty, ActiveMarketListingDTO>()
                 .ForMember(d => d.MunicipalityName, o => o.MapFrom(s => s.Municipality.MunicipalityName));
 
+            // Christian
+            CreateMap<RealEstateAgent, RealEstateAgentUpdateDTO>();
+
+            // Christian
+            CreateMap<RealEstateAgentUpdateDTO, RealEstateAgent>();
+
         }
     }
 }
