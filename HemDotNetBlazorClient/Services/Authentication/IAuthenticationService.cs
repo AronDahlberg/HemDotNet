0c5376d65e5084714b87bd5dba3a087a04a3ed26
Adam Khadi <khadi.adam@hotmail.se>
2025-04-28 13:06:12 +0200

    Added reg, login

---
+++ HemDotNetBlazorClient/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using HemDotNetBlazorClient.Services.Base;
+
+namespace IdentityDemoClient.Services.Authentication
+{
+    /* Coder: Adam, Participants: All */
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(LoginUserDto loginModel);
+        public Task Logout();
+    }
+}
