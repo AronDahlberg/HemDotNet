47a6f3a1a4640872b0ff5af16994e58fb2ff2ca5
Allan Crépin <alllan2107@gmail.com>
2025-04-30 20:34:03 +0200

    Added MarketPropertySearchDto, for coming search/filter endpoint

---
+++ HemDotNetWebApi/DTO/MarketPropertySearchDto.cs
@@ -0,0 +1,15 @@
+﻿using HemDotNetWebApi.Common;
+
+namespace HemDotNetWebApi.DTO
+{
+    // Allan
+    public class MarketPropertySearchDto
+    {
+        public string? Area { get; set; }
+        public List<PropertyCategory>? SelectedTypes { get; set; } = new();
+        public int? MinRooms { get; set; }
+        public double? MinArea { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public bool? NewProduction { get; set; }
+    }
+}
