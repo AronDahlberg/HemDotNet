b290a4fc693cec61f640d4aaf34783ff57e4fa7a
Chris <christian.bang@gmail.com>
2025-05-07 14:41:05 +0200

    more agency info on agent

--- HemDotNetWebApi/Mapping/AutomapperProfile.cs
+++ HemDotNetWebApi/Mapping/AutomapperProfile.cs
@@ -111,6 +111,10 @@ namespace HemDotNetWebApi.Mapping
                 opt => opt.MapFrom(src => src.RealEstateAgentAgency.RealEstateAgencyName))
             .ForMember(dest => dest.RealEstateAgentAgencyId,
                 opt => opt.MapFrom(src => src.RealEstateAgentAgency.RealEstateAgencyId))
+            .ForMember(dest => dest.RealEstateAgencyLogoUrl,
+                opt => opt.MapFrom(src => src.RealEstateAgentAgency.RealEstateAgencyLogoUrl))
+            .ForMember(dest => dest.RealEstateAgencyPresentation,
+                opt => opt.MapFrom(src => src.RealEstateAgentAgency.RealEstateAgencyPresentation))
             .ForMember(dest => dest.PropertyIds,
                 opt => opt.MapFrom(src => src.RealEstateAgentProperties.Select(p => p.MarketPropertyId)));
         }
