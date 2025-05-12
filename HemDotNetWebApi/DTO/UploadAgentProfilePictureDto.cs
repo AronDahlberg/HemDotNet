648f0ba2a3dbe45ed3b0843763a93d02dec9e819
Allan Crépin <alllan2107@gmail.com>
2025-05-12 14:31:18 +0200

    Added dto for uploading picture

---
+++ HemDotNetWebApi/DTO/UploadAgentProfilePictureDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HemDotNetWebApi.DTO
+{
+    public class UploadAgentProfilePictureDto
+    {
+        [Required]
+        public string AgentId { get; set; } // Optional if you only allow current user
+    }
+}
