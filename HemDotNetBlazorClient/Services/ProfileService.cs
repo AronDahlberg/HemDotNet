d334f532ed564e71e503d2f8107f6382862ab068
Allan Crépin <alllan2107@gmail.com>
2025-05-12 19:56:14 +0200

    Create service to register when user updates picture and update navbar

---
+++ HemDotNetBlazorClient/Services/ProfileService.cs
@@ -0,0 +1,27 @@
+﻿namespace HemDotNetBlazorClient.Services
+{
+    // Allan
+    public class ProfileService
+    {
+        private string _currentUserProfilePicture;
+
+        public event Action ProfilePictureChanged;
+
+        public string CurrentUserProfilePicture
+        {
+            get => _currentUserProfilePicture;
+            set
+            {
+                _currentUserProfilePicture = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => ProfilePictureChanged?.Invoke();
+
+        public void UpdateProfilePicture(string newImageUrl)
+        {
+            CurrentUserProfilePicture = newImageUrl;
+        }
+    }
+}
