502b0ee69907ce4a57a789bb14bc4a1f30facad0
Allan Crépin <alllan2107@gmail.com>
2025-05-13 11:38:10 +0200

    Added Dto for agency creation

---
+++ HemDotNetWebApi/DTO/AgencyCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HemDotNetWebApi.DTO
+{
+    public class AgencyCreateDto
+    {
+        [MaxLength(100)]
+        public string RealEstateAgencyName { get; set; }
+        public string RealEstateAgencyPresentation { get; set; }
+        public string RealEstateAgencyLogoUrl { get; set; }
+        public string RealEstateAgencyMunicipality { get; set; }
+    }
+}
