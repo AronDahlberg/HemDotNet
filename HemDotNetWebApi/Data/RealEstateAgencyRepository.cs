03a165ca9e35866ee96df59d77d2c6b5d688cf54
Allan Crépin <alllan2107@gmail.com>
2025-05-13 20:26:35 +0200

    Instead of putting agents from a deleted agency in wait list, we delete them. Putting them in wait list makes some properties belong to the agency "wait list" otherwise; and in real life, if an agency closes its doors, the agents are out of a job. If they work for another agency, that agency will create a new account for them at hemdotnet

--- HemDotNetWebApi/Data/RealEstateAgencyRepository.cs
+++ HemDotNetWebApi/Data/RealEstateAgencyRepository.cs
@@ -26,30 +26,44 @@ namespace HemDotNetWebApi.Data
         // Allan
         public async Task<bool> DeleteAgency(int id)
         {
-            // We want to delete an agency, but the agency has agents; what to do?
-            // here, we decide to assign those agents to the wait list.
+            // Options 1: we dont delete the agents linked to an agency, we move them to the wait list
+
+            //var agency = await _context.RealEstateAgencies
+            //    .Include(a => a.RealEstateAgencyAgents)
+            //    .FirstOrDefaultAsync(a => a.RealEstateAgencyId == id);
+
+            //if (agency == null)
+            //    return false;
+
+            //var waitListAgency = await _context.RealEstateAgencies
+            //    .FirstOrDefaultAsync(a => a.RealEstateAgencyName == "Wait List");
+
+            //if (waitListAgency == null)
+            //    throw new InvalidOperationException("Wait List agency not found. Please create one before deleting agencies.");
+
+
+            //foreach (var agent in agency.RealEstateAgencyAgents)
+            //{
+            //    agent.RealEstateAgentAgencyId = waitListAgency.RealEstateAgencyId;
+            //}
+
+            //_context.RealEstateAgencies.Remove(agency);
+
+            //await _context.SaveChangesAsync();
+            //return true;
+
+            // Option 2, we delete the agents and their properties
+
             var agency = await _context.RealEstateAgencies
-                .Include(a => a.RealEstateAgencyAgents)
-                .FirstOrDefaultAsync(a => a.RealEstateAgencyId == id);
+            .Include(a => a.RealEstateAgencyAgents)
+            .FirstOrDefaultAsync(a => a.RealEstateAgencyId == id);
 
-            if (agency == null)
-                return false;
+                    if (agency == null)
+                        return false;
 
-            var waitListAgency = await _context.RealEstateAgencies
-                .FirstOrDefaultAsync(a => a.RealEstateAgencyName == "Wait List");
-
-            if (waitListAgency == null)
-                throw new InvalidOperationException("Wait List agency not found. Please create one before deleting agencies.");
-
-            
-            foreach (var agent in agency.RealEstateAgencyAgents)
-            {
-                agent.RealEstateAgentAgencyId = waitListAgency.RealEstateAgencyId;
-            }
-
-            _context.RealEstateAgencies.Remove(agency);
-
-            await _context.SaveChangesAsync();
+                    _context.RealEstateAgents.RemoveRange(agency.RealEstateAgencyAgents);
+                    _context.RealEstateAgencies.Remove(agency);
+                    await _context.SaveChangesAsync();
             return true;
         }
     }
